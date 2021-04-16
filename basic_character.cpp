5e00cc35ca53a80445faf6618e49eb7f513d3746
Eli Huebner <elimhuebner@gmail.com>
2021-04-16 15:53:20 -0400

    declares variables for character stats and initializes them from user input

--- basic_character.cpp
+++ basic_character.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
+#include <string>
 
 int main () {
     std::cout << "Enter, in order, your character's name followed by their strength, dexterity, constitution, intelligence, wisdom, and charisma. All separated by spaces.\n";
+
+    std::string name;
+    int str, dex, con, smart, wis, comm;
+
+    std::cin >> name >> str >> dex >> con >> smart >> wis >> comm;
+
 }
