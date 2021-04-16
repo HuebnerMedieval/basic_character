f160822e79cb6e375e1ec4ed5227a075d6b5e12e
Eli Huebner <elimhuebner@gmail.com>
2021-04-16 15:48:23 -0400

    prompts user to input character name and stats

---
+++ basic_character.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+int main () {
+    std::cout << "Enter, in order, your character's name followed by their strength, dexterity, constitution, intelligence, wisdom, and charisma. All separated by spaces.\n";
+}
